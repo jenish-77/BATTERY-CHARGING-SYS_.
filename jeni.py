e6df53c1a9f1fc89effb2f67807ac2f54cde2c17
jenish-77 <jenishkj777@gmail.com>
2025-08-01 12:28:27 +0530

    Update jeni.py

--- jeni.py
+++ jeni.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 # Ensure matplotlib backend works well with Streamlit
 plt.switch_backend('agg')
