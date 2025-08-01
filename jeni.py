a8cb107368537880218761992bb322d6f4cac7ca
jenish-77 <jenishkj777@gmail.com>
2025-08-01 12:11:27 +0530

    Update jeni.py

--- jeni.py
+++ jeni.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotliv.pyplot as plt
+import matplotlib.pyplot as plt
 
 # ------------------------ Configuration ------------------------ #
 battery_types = {
